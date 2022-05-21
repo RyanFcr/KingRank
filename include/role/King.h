fd3c1a3e7fee433dcb66b29e53018d7ad49a0c84
RyanFcr <3200103626@zju.edu.cn>
2022-05-21 20:52:42 +0800

    包和King的初始化

--- include/role/King.h
+++ include/role/King.h
@@ -8,24 +8,25 @@
 #include "map/Map.h"
 #include "text/TextGen.h"
 
-class King : public Role {
+class King : public Role
+{
 public:
-    King(string name, Position position, int territoryId, int level = 1, int maxHP = 100, int HP = 100, 
-        int maxMP = 100, int MP = 100, int experience = 0,int money = 0) 
-        : Role(name, position, level, maxHP, HP, maxMP, MP), experience(experience), territoryId(territoryId),money(money) {
-        
+    King(string name, Position position, int territoryId, Bag bag,int attack = 1,  int level = 1, int maxHP = 100, int HP = 100,
+         int maxMP = 100, int MP = 100,int experience = 0, int money = 0)
+        : Role(name, position, attack, level, maxHP, HP, maxMP, MP), experience(experience), territoryId(territoryId), money(money), bag(bag){
     }
     void ShowMap(const Map &m);
     void GoUp(const Map &m);
     void GoDown(const Map &m);
     void GoLeft(const Map &m);
     void GoRight(const Map &m);
+
 private:
-    int experience; //当前的经验值
+    int experience;  //当前的经验值
     int territoryId; // 领地编号
-    int money;//拥有的金币量
+    int money;       //拥有的金币量
     Bag bag;
-    vector<Skill*> skills;
+    vector<Skill *> skills;
 };
 
 #endif // KING_H_
