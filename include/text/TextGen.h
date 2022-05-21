7442d52ada58e21e0fae0ea121d4b74cac8bd024
RyanFcr <3200103626@zju.edu.cn>
2022-05-21 20:23:29 +0800

    修改了打印map

--- include/text/TextGen.h
+++ include/text/TextGen.h
@@ -3,41 +3,106 @@
 
 #include <iomanip>
 #include <iostream>
-using std::ostream; using std::string; 
-using std::cout; using std::cin;
+using std::cin;
+using std::cout;
+using std::ostream;
+using std::string;
 
 #include "common/Macro.h"
 #include "map/Scene.h"
 #include "map/Map.h"
 #include "common/Global.h"
 
-template<int ansiStyle = 0>
-ostream& style(ostream& out) { 
-    return out << "\033[" << ansiStyle << "m"; 
+template <int ansiStyle = 0>
+ostream &style(ostream &out)
+{
+    return out << "\033[" << ansiStyle << "m";
 }
 
 /// \brief Generate different styles of texts
-class TextGen {
+class TextGen
+{
 public:
     static void Init();
     static void PrintTitle();
     static void PrintDirection(const Map &m, const Position &p);
-    
-    template<int ansiStyle = 0>
-    static void Print(string s, string end = "\n") {
+
+    template <int ansiStyle = 0>
+    static void Print(string s, string end = "\n")
+    {
         cout << style<ansiStyle> << s << style<plain> << end;
     }
+    template <int ansiStyle = 0>
+    static void PrintNoEnd(string s, string end = "\n")
+    {
+        cout << style<ansiStyle> << s << style<plain>;
+    }
 
-    template<int ansiStyle = 0>
-    static void PrintCenter(string s, int totalLength) {
+    template <int ansiStyle = 0>
+    static void PrintCenter(string s, int totalLength)
+    {
         int len = s.length();
-        if (len <= totalLength) {
-            for (int i = 0; i < (totalLength - len) / 2; i++) {
+        if (len <= totalLength)
+        {
+            for (int i = 0; i < (totalLength - len) / 2; i++)
+            {
                 cout << " ";
             }
         }
         Print<ansiStyle>(s);
     }
+    template <int ansiStyle = 0>
+    static void PrintTwo(string s1, string s2, int totalLength, string end = "\n")
+    {
+        cout << style<ansiStyle> << s1 << style<plain>;
+        int len1 = s1.length();
+        int len2 = s2.length();
+        if (len2 <= totalLength)
+        {
+            for (int i = 0; i < (totalLength - len2) / 2 - len1; i++)
+            {
+                cout << " ";
+            }
+        }
+        Print<ansiStyle>(s2);
+    }
+    template <int ansiStyle = 0>
+    static void PrintThree(string s1, string s2, string s3, int totalLength, string end = "\n")
+    {
+        cout << style<ansiStyle> << s1 << style<plain>;
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+        if (len2 <= totalLength)
+        {
+            for (int i = 0; i < (totalLength  - len2) / 2 - len1; i++)
+            {
+                cout << " ";
+            }
+        }
+        PrintNoEnd<ansiStyle>(s2);
+        if (len3 <= totalLength)
+        {
+            for (int i = 0; i < (totalLength - len2 ) / 2 - len3; i++)
+            {
+                cout << " ";
+            }
+        }
+        Print<ansiStyle>(s3);
+    }
+    // template <int ansiStyle = 0>
+    // static void PrintTail(string s, int totalLength)
+    // {
+    //     int len = s.length();
+    //     if (len <= totalLength)
+    //     {
+    //         for (int i = 0; i < (totalLength - len) ; i++)
+    //         {
+    //             cout << " ";
+    //         }
+    //     }
+    //     Print<ansiStyle>(s);
+    // }
 
     static string Input();
 };
--- src/text/TextGen.cpp
+++ src/text/TextGen.cpp
@@ -2,16 +2,17 @@
 #include <wchar.h>
 
 #ifdef _WIN32
-    #include <Windows.h>
+#include <Windows.h>
 #endif
 
-void TextGen::Init() {
+void TextGen::Init()
+{
 #ifdef _WIN32
-    //http://m.blog.csdn.net/article/details?id=52789570
+    // http://m.blog.csdn.net/article/details?id=52789570
     //控制台显示乱码纠正
-   // system("chcp 65001"); //设置字符集（使用SetConsoleCP(65001)设置无效，原因未知）
-    SetConsoleOutputCP (65001);
-    CONSOLE_FONT_INFOEX info = { 0 }; // 以下设置字体来支持中文显示。
+    // system("chcp 65001"); //设置字符集（使用SetConsoleCP(65001)设置无效，原因未知）
+    SetConsoleOutputCP(65001);
+    CONSOLE_FONT_INFOEX info = {0}; // 以下设置字体来支持中文显示。
     info.cbSize = sizeof(info);
     info.dwFontSize.Y = 16; // leave X as zero
     info.FontWeight = FW_NORMAL;
@@ -20,7 +21,8 @@ void TextGen::Init() {
 #endif
 }
 
-void TextGen::PrintTitle() {
+void TextGen::PrintTitle()
+{
     Print<title>("██╗  ██╗██╗███╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗██╗  ██╗");
     Print<title>("██║ ██╔╝██║████╗  ██║██╔════╝ ██╔══██╗██╔══██╗████╗  ██║██║ ██╔╝");
     Print<title>("█████╔╝ ██║██╔██╗ ██║██║  ███╗██████╔╝███████║██╔██╗ ██║█████╔╝");
@@ -29,9 +31,11 @@ void TextGen::PrintTitle() {
     Print<title>("╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝");
 }
 
-void TextGen::PrintDirection(const Map &m, const Position &p) {
+void TextGen::PrintDirection(const Map &m, const Position &p)
+{
     /// @attention high coupling!
-    if (p.fieldX >= 0 && p.fieldX < m.GetRowNum() && p.fieldY >= 0 && p.fieldY < m.GetColNum(p.fieldX)) {
+    if (p.fieldX >= 0 && p.fieldX < m.GetRowNum() && p.fieldY >= 0 && p.fieldY < m.GetColNum(p.fieldX))
+    {
         // Field Name
         string centerFieldName{m.GetField(p.fieldX, p.fieldY).GetName()};
         string leftFieldName{centerFieldName}, rightFieldName{centerFieldName};
@@ -40,13 +44,15 @@ void TextGen::PrintDirection(const Map &m, const Position &p) {
         // Coordinate
         string leftCo{""}, rightCo{""}, upCo{""}, downCo{""};
         string centerCo = "(" + std::to_string(p.sceneX) + "," + std::to_string(p.sceneY) + ")";
-        
+
         // left
-        if (p.sceneX - 1 < 0 && p.fieldX == 0) {
+        if (p.sceneX - 1 < 0 && p.fieldX == 0)
+        {
             leftFieldName = "";
             leftCo = "";
         }
-        else if (p.sceneX - 1 < 0) {
+        else if (p.sceneX - 1 < 0)
+        {
             leftFieldName = m.GetField(p.fieldX - 1, p.fieldY).GetName();
             leftCo = "(" + std::to_string(fieldSize - 1) + "," + std::to_string(p.sceneY) + ")";
         }
@@ -54,11 +60,13 @@ void TextGen::PrintDirection(const Map &m, const Position &p) {
             leftCo = "(" + std::to_string(p.sceneX - 1) + "," + std::to_string(p.sceneY) + ")";
 
         // right
-        if (p.sceneX + 1 >= fieldSize && p.fieldX == m.GetColNum(p.fieldX) - 1) {
+        if (p.sceneX + 1 >= fieldSize && p.fieldX == m.GetColNum(p.fieldX) - 1)
+        {
             rightFieldName = "";
             rightCo = "";
         }
-        else if (p.sceneX + 1 >= fieldSize) {
+        else if (p.sceneX + 1 >= fieldSize)
+        {
             rightFieldName = m.GetField(p.fieldX + 1, p.fieldY).GetName();
             rightCo = "(" + std::to_string(0) + "," + std::to_string(p.sceneY) + ")";
         }
@@ -66,11 +74,13 @@ void TextGen::PrintDirection(const Map &m, const Position &p) {
             rightCo = "(" + std::to_string(p.sceneX + 1) + "," + std::to_string(p.sceneY) + ")";
 
         // down
-        if (p.sceneY - 1 < 0 && p.fieldY == 0) {
+        if (p.sceneY - 1 < 0 && p.fieldY == 0)
+        {
             downFieldName = "";
             downCo = "";
         }
-        else if (p.sceneY - 1 < 0) {
+        else if (p.sceneY - 1 < 0)
+        {
             downFieldName = m.GetField(p.fieldX, p.fieldY - 1).GetName();
             downCo = "(" + std::to_string(p.sceneX) + "," + std::to_string(fieldSize - 1) + ")";
         }
@@ -78,53 +88,107 @@ void TextGen::PrintDirection(const Map &m, const Position &p) {
             downCo = "(" + std::to_string(p.sceneX) + "," + std::to_string(p.sceneY - 1) + ")";
 
         // up
-        if (p.sceneY + 1 >= fieldSize && p.fieldY == m.GetRowNum() - 1) {
+        if (p.sceneY + 1 >= fieldSize && p.fieldY == m.GetRowNum() - 1)
+        {
             upFieldName = "";
             upCo = "";
         }
-        else if (p.sceneY + 1 >= fieldSize) {
+        else if (p.sceneY + 1 >= fieldSize)
+        {
             upFieldName = m.GetField(p.fieldX, p.fieldY + 1).GetName();
             upCo = "(" + std::to_string(p.sceneX) + "," + std::to_string(0) + ")";
         }
         else
-            upCo = "(" + std::to_string(p.sceneX) + "," + std::to_string(p.sceneY + 1) + ")"; 
-        
+            upCo = "(" + std::to_string(p.sceneX) + "," + std::to_string(p.sceneY + 1) + ")";
+
         // draw the map
-        // up
-        if (upFieldName != "") {
-            PrintCenter(upFieldName, maxTerminalLenghth);
-            PrintCenter(upCo, maxTerminalLenghth);
-            PrintCenter("test state", maxTerminalLenghth);
-            PrintCenter("⇑", maxTerminalLenghth);
-            PrintCenter("⇑", maxTerminalLenghth);
-        }
-        // left
-        if (leftFieldName != "") {
 
-        }
-        Print(centerFieldName);
-        Print(centerCo);
-        Print<buff>("test");
-        // right
-        if (rightFieldName != "") {
+        // if left can go
+        if (leftFieldName != "")
+        {
+            // up
+            if (upFieldName != "")
+            {
+                PrintCenter(upFieldName, maxTerminalLenghth);
+                PrintCenter(upCo, maxTerminalLenghth);
+                PrintCenter("up state", maxTerminalLenghth);
+                PrintCenter("⇧", maxTerminalLenghth);
+                PrintCenter("⇧", maxTerminalLenghth);
+            }
 
-        }
-        // down
-        if (downFieldName != "") {
+            // right
+            if (rightFieldName != "")
+            {
+                PrintThree(leftFieldName, centerFieldName, rightFieldName, maxTerminalLenghth);
+                PrintThree(leftCo, centerCo, rightCo, maxTerminalLenghth);
+                PrintThree("left state", "⇦ ⇦ center ⇨ ⇨", "right state", maxTerminalLenghth);
+            }
+            else
+            {
+                PrintTwo(leftFieldName, centerFieldName, maxTerminalLenghth);
+                PrintTwo(leftCo, centerCo, maxTerminalLenghth);
+                PrintTwo("left state", "⇦ ⇦ center", maxTerminalLenghth);
+            }
 
+            // down
+            if (downFieldName != "")
+            {
+                PrintCenter(downFieldName, maxTerminalLenghth);
+                PrintCenter(downCo, maxTerminalLenghth);
+                PrintCenter("down state", maxTerminalLenghth);
+                PrintCenter("⇩", maxTerminalLenghth);
+                PrintCenter("⇩", maxTerminalLenghth);
+            }
+        }
+        else
+        {
+            // up
+            if (upFieldName != "")
+            {
+                Print(upFieldName);
+                Print(upCo);
+                Print("up state");
+                Print("⇧");
+                Print("⇧");
+            }
+
+            if (rightFieldName != "")
+            {
+                PrintTwo(centerFieldName, rightFieldName, maxTerminalLenghth);
+                PrintTwo(centerCo, rightCo, maxTerminalLenghth);
+                PrintTwo("center ⇨ ⇨", "right state", maxTerminalLenghth);
+            }
+            else
+            {
+                Print(centerFieldName);
+                Print(centerCo);
+                Print("center");
+            }
+            // down
+            if (downFieldName != "")
+            {
+                Print(downFieldName);
+                Print(downCo);
+                Print("down state");
+                Print("⇩");
+                Print("⇩");
+            }
         }
     }
-    else {
+    else
+    {
         Print<warning>("You has entered an unknown field!");
-    }                           
+    }
 }
 
-string TextGen::Input() {
+string TextGen::Input()
+{
     string input;
     Print("> ", "");
     getline(cin, input);
 
-    if (input == "quit") {
+    if (input == "quit")
+    {
         Print("Bye!");
         exit(0);
     }
